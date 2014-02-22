8cff1b1dc8515722a8dedd9424ad8eb44068baae
Gabriel Horner <gabriel.horner@gmail.com>
2014-02-22 10:44:43 -0500

    create a reusable cmd invokation

--- src/lt/plugins/ltfiles/browse.cljs
+++ src/lt/plugins/ltfiles/browse.cljs
@@ -22,15 +22,23 @@
               :desc "ltfiles: opens url under cursor in another tabset and browser"
               :exec tab-open-current-url})
 
+;; Until lt.objs.proc/exec works: passing {:command "" :obj ""} to it didn't work
+(defn sh [cmd]
+  ((.-exec (js/require "child_process"))
+   cmd
+   (fn [err stdout stderr]
+     (when (seq stdout) (println "STDOUT: " stdout))
+     (when (seq stderr) (println "STDERR: " stderr)))))
+
+;; OSX-specific for now
 (defn system-open-current-url []
-  (let [exec (.-exec (js/require "child_process"))]
-    ;; OSX-specific for now
-    (exec (str "open " (util/current-word))
-          (fn [err stdout stderr]
-            (when (seq stdout) (println "STDOUT: " stdout))
-            (when (seq stderr) (println "STDERR: " stderr))))))
+   (sh (str "open " (util/current-word))))
 
 (cmd/command {:command :ltfiles.system-open-current-url
               :desc "ltfiles: opens url under cursor in system browser"
               :exec system-open-current-url})
+
+(comment
+  (sh "git status")
+  )
 
