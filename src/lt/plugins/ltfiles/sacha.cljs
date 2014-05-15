f9619bcc3bf015c671b676d5014b1a5d5cf62dde
Gabriel Horner <gabriel.horner@gmail.com>
2014-05-15 00:38:06 -0400

    enable backslash escaping of tags

--- src/lt/plugins/ltfiles/sacha.cljs
+++ src/lt/plugins/ltfiles/sacha.cljs
@@ -32,12 +32,19 @@
          (not (desc-node? next)))))
 
 (def tag-prefix "#")
-(def tag-pattern (re-pattern (str tag-prefix "[^ \\t\\n:.,?]+")))
+
+;; This regex returns pairs of matches but only the latter is useful. This
+;; is a necessary evil caused by no negative-lookbehind in JS
+(def tag-pattern
+  "Regex for pulling out tags with tag-prefix. To escape having a tag parsed,
+  put a backslash before it e.g. \\#escaped"
+  (re-pattern (str "(?:[^\\\\]|^)"
+                   "(" tag-prefix "[^ \\t\\n:.,?]+" ")")))
 
 (defn text->tags [text]
   (map
    #(subs % 1)
-   (re-seq tag-pattern text)))
+   (map second (re-seq tag-pattern text))))
 
 (defn add-node-with-tags [nodes node tags]
   (conj nodes (assoc node :tags tags)))
